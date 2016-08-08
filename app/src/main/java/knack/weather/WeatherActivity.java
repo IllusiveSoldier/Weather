08e62748a77aa5869b2b43c80440f9f26bda5f27
IllusiveSoldier <lyamkin2011@gmail.com>
2016-08-09 01:18:20 +0300

    remove geolocation

--- app/src/main/java/knack/weather/WeatherActivity.java
+++ app/src/main/java/knack/weather/WeatherActivity.java
@@ -2,7 +2,6 @@ package knack.weather;
 
 import android.os.Bundle;
 
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
 public class WeatherActivity extends AppCompatActivity
@@ -15,19 +14,13 @@ public class WeatherActivity extends AppCompatActivity
 
         if (findViewById(R.id.FragmentContainer) != null)
         {
-
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
             if (savedInstanceState != null)
             {
                 return;
             }
 
-            // Create a new Fragment to be placed in the activity layout
             WeatherFragment weatherFragment = new WeatherFragment();
 
-            // Add the fragment to the 'fragment_container' FrameLayout
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.FragmentContainer, weatherFragment).commit();
         }
